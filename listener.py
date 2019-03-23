7baa9e8adedc3753b5b57a8ba5b34b35ffd76553
shivaco <ezboost322@gmail.com>
2019-03-24 00:28:48 +0600

    Added Discord webhook support

--- listener.py
+++ listener.py
@@ -1,10 +1,17 @@
-import sys, datetime, json
+import sys, datetime, json, re, requests
+from discord import Webhook, RequestsWebhookAdapter, Embed, Color
 from flask import Flask, request, abort, jsonify
 from pymongo import MongoClient
 import os
 
 with open("settings.json") as settings:
-    webhook_password = json.load(settings)['webhook_password']
+    data = json.load(settings)
+    webhook_password = data['webhook_password']
+    discord_webhook = data['discord_webhook_url']
+    if discord_webhook != "":
+        webhook_id = int(re.search(r"\d+", discord_webhook).group())
+        webhook_token = re.search(r"(\w+)$", discord_webhook).group()
+
 
 mongo = MongoClient()
 app = Flask(__name__)
@@ -17,11 +24,23 @@ def test():
 def webhook():
     sys.stdout.flush()
     if request.headers.get('Authorization') == webhook_password:
-        mongo.voters.vote.insert_one({'type': request.json.get('type'),
-            'user': request.json.get('user'),
-            'bot': request.json.get('bot'),
-            'weekend': request.json.get('isWeekend'),
-            'time': datetime.datetime.utcnow()})
+        user_id = request.json.get('user')
+        bot_id = request.json.get('bot') # ID of the bot that was upvoted 
+        request_type = request.json.get('type')
+        weekend_status = request.json.get('isWeekend')
+        now = datetime.datetime.utcnow()
+        mongo.voters.vote.insert_one({
+            'type': request_type,
+            'user': user_id,
+            'bot': bot_id,
+            'weekend': weekend_status,
+            'time': now
+            })
+        if discord_webhook != "":
+            embed_title = "Test" if request_type == 'test' else 'New upvote!'
+            embed = Embed(title=embed_title, description=f"**Upvoter: <@{user_id}>** ({user_id})\n**Upvoted bot:** <@{bot_id}> ({bot_id})", timestamp=datetime.datetime.utcnow(), color=Color.green())
+            webhook = Webhook.partial(webhook_id, webhook_token, adapter=RequestsWebhookAdapter())
+            webhook.send(embed=embed)
         return '', 200
     else:
         abort(400)
@@ -29,3 +48,4 @@ def webhook():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+    mongo.close()
